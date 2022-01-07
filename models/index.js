c205e0d9749e813dcf27e5db71b152a5b89b4d47
muktar ahmed <muktar.ahmed13@yahoo.com>
2022-01-07 01:39:45 -0600

    added comments router

--- models/index.js
+++ models/index.js
@@ -1,6 +1,51 @@
-const User = require('./User');
+const Vote = require('./vote');
+const User = require('./user');
 const Post = require('./post')
+const Comment = require('./comment')
+
 // create associations
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+  });
+  
+  Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+  });
+  Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Vote, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+  });
 User.hasMany(Post, {
     foreignKey: 'user_id'
   });
@@ -9,4 +54,4 @@ User.hasMany(Post, {
     foreignKey: 'user_id',
   });
   
-module.exports = { User, Post };
+module.exports = { User, Post, Vote, Comment };
