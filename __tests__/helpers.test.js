dd08003fc4e622dd3aadcf098ff650b52d1583b5
muktar ahmed <muktar.ahmed13@yahoo.com>
2022-01-13 23:58:11 -0600

    added partials and testing

---
+++ __tests__/helpers.test.js
@@ -0,0 +1,28 @@
+const {format_date,format_plural,format_url} = require('../utils/helpers');
+
+test('format_date() returns a date string', () => {
+    const date = new Date('2020-03-20 16:12:03');
+  
+    expect(format_date(date)).toBe('3/20/2020');
+  });
+
+  test('format_plural() retutn plural word',()=>{
+    const word = ('tiger');
+
+    expect(format_plural(word, 2)).toBe('tigers')
+  })
+
+  test('format_plural() retutn plural word',()=>{
+    const word = ('lion');
+
+    expect(format_plural(word, 1)).toBe('lion')
+  })
+  test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/page/1');
+    const url2 = format_url('https://coolstuff.com/abcdefg/');
+    const url3 = format_url('https://google.com?q=hello');
+  
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('coolstuff.com');
+    expect(url3).toBe('google.com');
+  });
